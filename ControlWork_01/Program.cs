92b4f239b6d233599f4a201c3e3e712e4c4a1f51
Dmitri5 <dmitriprogram@gmail.com>
2022-08-22 15:24:59 +0700

    Start write code, added method InputNumbers

---
+++ ControlWork_01/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Программа выведет на экран только слова состоящии из трех и менее букв.");
+
+string[] arrayOfStrings = new string[] { };
+
+int n = InputNumbers("Введите количество элементов массива: ");
+arrayOfStrings = new string[n];
+for (int i = 0; i < arrayOfStrings.Length; i++)
+{
+    Console.Write($" Введите {i + 1} строку: ");
+    arrayOfStrings[i] = Console.ReadLine();
+}
+
+int InputNumbers(string input) // Метод, пользователь задает количество элементов в новом массиве
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
