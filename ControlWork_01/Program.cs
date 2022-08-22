7089477b5f017c2e1ec51b3dea48a78e7cb35204
Dmitri5 <dmitriprogram@gmail.com>
2022-08-22 15:55:58 +0700

    Added methods FillNewArray and PrintArray

--- ControlWork_01/Program.cs
+++ ControlWork_01/Program.cs
@@ -17,6 +17,36 @@ int numbersItems = CheckArray(arrayOfStrings, lengthLimit);
 
 string[] newArrayOfStrings = new string[numbersItems];
 
+FillNewArray(arrayOfStrings, newArrayOfStrings, lengthLimit);
+
+Console.WriteLine($"{PrintArray(newArrayOfStrings)}");
+
+string PrintArray(string[] array) // Метод печатает массив.
+{
+    string result = string.Empty;
+    result = "[ ";
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += $"{array[i],1}";
+        if (i < array.Length - 1) result += ", ";
+    }
+    result += " ]";
+    return result;
+}
+
+void FillNewArray(string[] oldArray, string[] newArray, int lengthLimit) // Метод формирования нового массива строк
+{
+    int temp = 0;
+    for (int i = 0; i < oldArray.Length; i++)
+    {
+        if (oldArray[i].Length <= lengthLimit)
+        {
+            newArray[temp] = oldArray[i];
+            temp++;
+        }
+    }
+}
+
 int CheckArray(string[] array, int lengthLimit) // Метод подсчёта количества элементов, длина которых меньше или равна 3 символа.
 {
     int result = 0;
